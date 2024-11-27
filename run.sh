8222cf633294ae1ef25c94579673228fc9833b62
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-11-27 14:59:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 agentql init
-export AGENTQL_API_KEY=xJ2gYetiuPm2qbQl_h5ietTOPGaO38BsEUyuGcWDGaoNEcGhoBMMZA
-protonvpn init
-protonvpn c -r
+export
+AGENTQL_API_KEY=xJ2gYetiuPm2qbQl_h5ietTOPGaO38BsEUyuGcWDGaoNEcGhoBMMZA
+#protonvpn init
+#protonvpn c -r
 pytest -v -s test_agent.py
