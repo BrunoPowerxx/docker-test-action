137dd152a9bcf988f98eae9ea180c0456b07f01d
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2024-12-13 10:38:49 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 agentql init
 export
 AGENTQL_API_KEY=xJ2gYetiuPm2qbQl_h5ietTOPGaO38BsEUyuGcWDGaoNEcGhoBMMZA
-pytest -v -s test_ip.py
-echo "Starting HTTP server on port 8000 to serve the screenshot..."
-python3 -m http.server 8000 --directory /output to
+pytest -v -s test_bookies.py
+#echo "Starting HTTP server on port 8000 to serve the screenshot..."
+#python3 -m http.server 8000 --directory /output to
