37ca2ea72775a894083eecd184cefb520d8fe46f
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2025-02-19 20:43:17 +0200

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -5,7 +5,6 @@ import concurrent.futures
 import pandas as pd
 import agentql
 import random
-import asyncio
 import pytest
 import json
 import time
@@ -22,13 +21,13 @@ SPORTS_PAGE = """
 }        """
 URL = "https://www.supabets.co.za"
 
-async def click_game(game, browser):
-        context_2 = await browser.new_context()
-        subpage = agentql.wrap(await context_2.new_page())
-        await subpage.goto(URL, wait_until="networkidle")
-        await game.click()
-        result_url = subpage.url  # Extract the navigated URL
-        await subpage.close()
+ def test_click(game, browser):
+        context_2 = browser.new_context()
+        subpage = agentql.wrap(context_2.new_page())
+        subpage.goto(URL, wait_until="networkidle")
+        game.click()
+        result_url = subpage.url
+        subpage.close()
         
         return result_url
 
@@ -36,18 +35,18 @@ async def supabets_urls():
     display = Display(visible=False, size=(1920, 1080))
     display.start()
 
-    async with async_playwright() as p:
-        browser = await  p.chromium.launch(headless=False)
-        context = await browser.new_context()
-        page = agentql.wrap(await context.new_page())
+    with async_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = agentql.wrap(context.new_page())
 
-        await page.goto(URL, wait_until="networkidle")
-        homepage = await  page.query_elements(SPORTS_PAGE)
+        page.goto(URL, wait_until="networkidle")
+        homepage = page.query_elements(SPORTS_PAGE)
         games_cont = homepage.league_container.match_containers
         games = [homepage.league_container.match_containers[i].home for i in range(len(games_cont))]
         links = []
         for game in games:
-            await game.click()
+            game.click()
             link = page.url
             links.append(link)
             page.go_back()
@@ -57,6 +56,3 @@ async def supabets_urls():
         #print(f"loop took {end_time - start_time}")
 
     display.stop()
-
-def test_links():
-    asyncio.run(supabets_urls())
