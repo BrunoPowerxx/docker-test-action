d79fecb0ac1601ef528bccba8daed9f0efbf7106
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2025-02-19 23:08:54 +0200

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -37,19 +37,10 @@ def test_supabets():
         homepage = page.query_elements(SPORTS_PAGE)
         games_cont = homepage.league_container.match_containers
         links = []
-        for index in range(len(games_cont)):
-            print("Before click:", page.url)
-            game = homepage.league_container.match_containers[index].home
-            game.click()
-            page.wait_for_load_state("load")
-            print("After click:", page.url)
-            link = page.url
-            links.append(link)
-            page.go_back()
-            page.wait_for_load_state("load")
-        for link in links:
-            
-            print("Collected URL:", link)
-        #print(f"loop took {end_time - start_time}")
+        game = homepage.league_group_container.match_containers[0].home
+        game.click()
+        page.wait_for_load_state("load")
+        print("After click:", page.url)
+        page.wait_for_load_state("load")
 
     display.stop()
