7be92862fbf8fe9c2c1f13c3457aec225b3de3ac
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2025-03-16 02:28:09 +0200

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -1,56 +1,3 @@
-from playwright.sync_api import sync_playwright
-from pyvirtualdisplay import Display
-from time import perf_counter
-import concurrent.futures
-import pandas as pd
-import agentql
-import random
-import pytest
-import json
-import time
-
-URL = "https://www.supabets.co.za"
-'''
-def test_supabets():
-    SPORTS_PAGE = """ 
-{
-   league_group_container {
-       match_containers[] {
-           league
-           home
-           away
-           day
-       }
-   }
-}        """
-    display = Display(visible=False, size=(1920, 1080))
-    display.start()
-
-    with sync_playwright() as p:
-        
-        browser = p.chromium.launch(headless=False)
-        context = browser.new_context()
-        page = agentql.wrap(context.new_page())
-
-        page.goto(URL)
-        page.wait_for_load_state("load")
-        page.screenshot(path="before.png", full_page=True)
-        homepage = page.query_elements(SPORTS_PAGE)
-        
-        home_loc = homepage.league_group_container.match_containers[0].home
-        if home_loc:
-            home_class = home_loc.get_attribute("class")
-            home_loc.click()
-            page.wait_for_load_state("load")
-            print("After click:", page.url)
-            page.screenshot(path="after.png", full_page=True)
-
-        #game = homepage.league_group_container.match_containers[0].home
-        #game.click()
-        browser.close
-    display.stop()
-'''
-
 from pyvirtualdisplay import Display
 import json
 import time
@@ -95,79 +42,8 @@ def get_response(page: Page):
   }
 }
     """
+    
     ODDS_PAGE = """
-{
-  subevent_details {
-    match_tracker {
-      home_v_away
-      day
-      time
-    }
-    list_cgq {
-      all
-      popular
-    }
-    subevent_panel {
-      head_to_head(1x2) {
-        market_type
-        home {
-          odd_type
-          odd_value
-        }
-        draw {
-          odd_type
-          odd_value
-        }
-        away {
-          odd_type
-          odd_value
-        }
-      }
-      first_goal("1st goal", odds values) {
-        market_type
-        None {
-          odd_type
-          odd_value
-        }
-        home {
-          odd_type
-          odd_value
-        }
-        away {
-          odd_type
-          odd_value
-        }
-      }
-      first_half_first_goal {
-        market_type
-        None {
-          odd_type
-          odd_value
-        }
-        home {
-          odd_type
-          odd_value
-        }
-        away {
-          odd_type
-          odd_value
-        }
-      }
-      first_half_away_over_under(0.5, odds vales) {
-        market_type
-        over {
-          odd_type
-          odd_value
-        }
-        under {
-          odd_type
-          odd_value
-        }
-      }
-    }
-  }
-}"""
-    ODDS = """
 {
   page_url
   team_names(home v away)
@@ -193,7 +69,7 @@ def get_response(page: Page):
         home_locator.click()
         page.screenshot(path=f"/app/shots/supa_{index}.png", full_page=True)
 
-        event = page.query_data(ODDS)
+        event = page.query_data(ODDS_PAGE)
         #ties = elements.league_group_container.league_containers.home[0:5]
         
         if event:  # Ensure event data is not empty
