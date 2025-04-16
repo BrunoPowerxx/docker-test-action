911ba1b8b1fe5dfc5049dc45f8f9698d5b381184
BrunoPowerxx <100509830+BrunoPowerxx@users.noreply.github.com>
2025-04-16 12:24:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 agentql init
-export AGENTQL_API_KEY=xJ2gYetiuPm2qbQl_h5ietTOPGaO38BsEUyuGcWDGaoNEcGhoBMMZA
+export AGENTQL_API_KEY=3xOoDfRXU-Wuc1eQeSqhjclCk-E9kb-nP4gAJEUfeOmZOJUhd0IEwg
 pytest -v -s test_bot.py
 #python3 -m http.server 8000 --directory /output to
